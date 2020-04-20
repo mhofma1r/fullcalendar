6f14081f482cfb6b1e6ac6aa46ea8f885bb376a8
Michael Hofmair <michael.hofmair@grandgarage.eu>
2020-04-20 16:24:01 +0200

    adapt calendar for additional MaintenanceEvent specific data

--- resources/js/components/Tool.vue
+++ resources/js/components/Tool.vue
@@ -10,7 +10,6 @@
             :plugins="calendarPlugins"
             :weekends="calendarWeekends"
             :events="calendarEvents"
-            :locale="fr"
             :header="{
                 left: 'prev,next today',
                 center: 'title',
@@ -18,90 +17,96 @@
             }"
         />
     </div>
+
 </template>
 
 <script>
+    import FullCalendar from "@fullcalendar/vue";
+    import dayGridPlugin from "@fullcalendar/daygrid";
+    import timeGridPlugin from "@fullcalendar/timegrid";
+    import interactionPlugin from "@fullcalendar/interaction";
+    import listPlugin from '@fullcalendar/list';
+    import Tooltip from 'tooltip.js';
 
-import FullCalendar from "@fullcalendar/vue";
-import dayGridPlugin from "@fullcalendar/daygrid";
-import timeGridPlugin from "@fullcalendar/timegrid";
-import interactionPlugin from "@fullcalendar/interaction";
-import listPlugin from '@fullcalendar/list';
-import Vue from 'vue';
-import Tooltip from 'tooltip.js';
+    export default {
 
-export default {
+        components: {
+            FullCalendar,
+        },
+        methods: {
+            handleDateClick(info) {
+                // console.log(info);
+            },
+            handleEventClick(info) {
+//
+            },
+            handleMouseEnter(info) {
+                 console.log(info.event);
+            },
+            handleMouseLeave(info) {
+                // console.log(info)
+            },
+            handleEventRender(info) {
+                this.tooltip = new Tooltip(info.el, {
+                    title: info.event.title + '<br/>' +
+                        '<p>Priorität: ' + info.event.extendedProps.prio + '</p>' +
+                        '<p>Status: ' + info.event.extendedProps.progress + '</p>',
+                    placement: 'right',
+                    trigger: 'hover',
+                    padding: 5,
+                    html: true,
+                    container: 'body'
+                });
 
-    components: {
-        FullCalendar
-    },
-    methods: {
-        handleDateClick(info) {
-            //console.log(info);
-        },
-        handleEventClick(info) {
-            //console.log(info);
-        },
-        handleMouseEnter(info) {
-            //console.log(info);
-        },
-        handleMouseLeave(info) {
-            //console.log(info);
-            this.tooltip = null;
-        },
-        handleEventRender(info) {
-            //console.log(info);
-            this.tooltip = new Tooltip(info.el, {
-                title:"<div><h3>bongo</h3><p>Typ: Ölwechsel</p><p>Priorität: HOCH</p></div>",
-                text:"bimbo",
-                placement: 'top',
-                trigger: 'hover',
-                html: true,
-                container: 'body'
-            });
-        }
-    },
-    tooltip(){
 
-    },
-    data() {
-        return {
-            tooltip: "torpedo",
-            calendarPlugins: [
-                dayGridPlugin,
-                timeGridPlugin,
-                interactionPlugin,
-                listPlugin
-            ],
-            calendarWeekends: true,
-            calendarEvents: [
-                { title: "Today", start: new Date() }
-            ]
-        };
-    },
-    mounted() {
-        console.log('mounted()')
-        axios.get('/horaceho/fullcalendar/events').then(response => {
-            this.events = response.data
-            if (this.events) {
-                this.calendarEvents = []
+            }
+        },
+        tooltip() {
+
+        },
+        data() {
+            return {
+                tooltip: "torpedo",
+                calendarPlugins: [
+                    dayGridPlugin,
+                    timeGridPlugin,
+                    interactionPlugin,
+                    listPlugin
+                ],
+                calendarWeekends: true,
+                calendarEvents: [
+                    {title: "Today", start: new Date('2022-12-22 00:00:00')}
+                ]
+            };
+        },
+        mounted() {
+            axios.get('/horaceho/fullcalendar/events').then(response => {
+                this.events = response.data
+                if (this.events) {
+                    this.calendarEvents = []
+                }
                 this.events.forEach((event) => {
+
                     this.calendarEvents.push({
-                        title:console.log(event),
+                        url: console.log(event),
+                        title:event.title,
+                        progress: event.progress,
+                        prio: event.priority,
+                        recurrence: event.recurring_in_days,
                         start: event.start_date,
                         end: event.end_date
                     })
                 })
-            }
-        });
-    },
-}
+
+            });
+        },
+    }
 
 </script>
 
 <style>
-@import "~@fullcalendar/core/main.css";
-@import "~@fullcalendar/daygrid/main.css";
-@import "~@fullcalendar/timegrid/main.css";
-@import "~@fullcalendar/list/main.css";
+    @import "~@fullcalendar/core/main.css";
+    @import "~@fullcalendar/daygrid/main.css";
+    @import "~@fullcalendar/timegrid/main.css";
+    @import "~@fullcalendar/list/main.css";
 </style>
---
+++ resources/js/components/popup.vue
@@ -0,0 +1,20 @@
+<template>
+    <v-dialog>
+        <v-btn slot="activator" class="success">Event details</v-btn>
+        <v-card>
+            <v-card-title>
+                bongo-test-title
+            </v-card-title>
+        </v-card>
+    </v-dialog>
+</template>
+
+<script>
+    export default {
+        name: "popup"
+    }
+</script>
+
+<style scoped>
+
+</style>
